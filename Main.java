bd91fcece912f72666cd2bdeffbf648a2b784829
MariaClaraSantosMarca <202302136@est.umss.edu>
2024-10-03 06:43:17 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+package metodos;
+
+/**
+@autor Maria Clara Santos Marca
+@codido sis: 202302136
+
+ * La clase Main es la clase de entrada del programa. Contiene el método principal que inicia la ejecución del programa.
+ */
+public class Main {
+
+    /**
+     * Método principal que inicia el programa creando una nueva instancia de
+     * la clase InterfazGrafica.
+     * 
+     * @param args Los argumentos de línea de comandos (no utilizados en este programa).
+     */
+    public static void main(String[] args) {
+        new InterfazGrafica(); // Inicializa la interfaz gráfica
+    }
+}
